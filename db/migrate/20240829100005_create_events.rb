252fafacce2817a5045490161e9f1c530b83710e
daichi1997 <tagaaan@gmail.com>
2024-08-29 19:07:24 +0900

    eventモデル、テーブルの作成

---
+++ db/migrate/20240829100005_create_events.rb
@@ -0,0 +1,13 @@
+class CreateEvents < ActiveRecord::Migration[7.0]
+  def change
+    create_table :events do |t|
+      t.references :user ,       null: false
+      t.string     :title,       null: false
+      t.text       :description, null: false
+      t.datetime   :start_time,  null: false
+      t.string     :location,    null: false
+      t.integer    :tag_id,      null: false
+      t.timestamps
+    end
+  end
+end
