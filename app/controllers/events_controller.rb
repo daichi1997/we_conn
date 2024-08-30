82dc30f0844cef9bf77036d6f7d5c5a47a19a22b
daichi1997 <tagaaan@gmail.com>
2024-08-30 17:08:31 +0900

    事前導入

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,51 @@
 class EventsController < ApplicationController
+  before_action :set_event, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
+    @events = Event.all
+  end
+
+  def show
+  end
+
+  def new
+    @event = current_user.events.build
+  end
+
+  def create
+    @event = current_user.events.build(event_params)
+    if @event.save
+      redirect_to @event, notice: 'Event was successfully created.'
+    else
+      render :new
+    end
+
+  end
+
+  def edit
+  end
+
+  def update
+    if @event.update(event_params)
+      redirect_to @event, notice: 'Event was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @event.destroy
+    redirect_to events_url, notice: 'Event was successfully destroyed.'
+  end
+
+  private
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  def event_params
+    params.require(:event).permit(:title, :description, :start_time,:location,:image).merge(user_id: current_user.id)
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,9 +1,11 @@
 class Event < ApplicationRecord
+  has_one_attached :image
+
   belongs_to :user
 
-  validates :title,        presence: true,
-  validates :description,        presence: true,
-  validates :stars_time,        presence: true,
-  validates :location,        presence: true,
+  validates :title,        presence: true
+  validates :description,        presence: true
+  validates :start_time,        presence: true
+  validates :location,        presence: true
   validates :tag_id,        presence: true, numericality:  { other_than: 1, message: "can't be blank" }
 end
