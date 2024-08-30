bfe09433e992dee9e597a11406b59f5d13cc0351
daichi1997 <tagaaan@gmail.com>
2024-08-30 15:01:41 +0900

    ログイン有無での表示の違い、ユーザー詳細ページの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,22 @@ class User < ApplicationRecord
          validates :name, presence: true, length: { minimum: 2, maximum: 10 }
          validates :password, presence: true, 
          length: { minimum: 6, message: 'は6文字以上で入力してください' },
-         format: { with: /\A[a-zA-Z0-9]+\z/}
-         validates :email, presence: true, uniqueness: true, 
-         format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
+         format: { with: /\A[a-zA-Z0-9]+\z/},
+         if: :password_required?
          has_many :events
+         validates :bio, length: { maximum: 300 }
+
+         def update_without_password(params, *options)
+          params.delete(:password)
+          params.delete(:password_confirmation)
+      
+          result = update(params, *options)
+          clean_up_passwords
+          result
+        end
+        
+        def password_required?
+          new_record? || password.present? || password_confirmation.present?
+        end
       
 end
