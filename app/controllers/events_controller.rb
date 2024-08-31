8208a6101f93820cef81b2a4c94acd66735c24ef
daichi1997 <tagaaan@gmail.com>
2024-08-31 12:34:50 +0900

    イチランページ修正

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,7 +3,7 @@ class EventsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    @events = Event.all
+    @events = Event.order(created_at: :desc)
   end
 
   def show
