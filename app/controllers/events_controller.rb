0ce421875a377b9d379277defb62bb41e4b70509
daichi1997 <tagaaan@gmail.com>
2024-09-01 22:38:12 +0900

    イベント詳細ページの実装

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,8 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :check_owner, only: [:edit, :update, :destroy]
+
 
   def index
     @events = Event.order(created_at: :desc)
@@ -33,15 +35,18 @@ class EventsController < ApplicationController
 
   def update
     if @event.update(event_params)
-      redirect_to @event, notice: 'Event was successfully updated.'
+      redirect_to @event, notice: 'イベントを更新しました。'
     else
       render :edit
     end
   end
 
   def destroy
-    @event.destroy
-    redirect_to events_url, notice: 'Event was successfully destroyed.'
+    if @event.destroy
+      redirect_to events_path, notice: 'イベントが正常に削除されました。'
+    else
+      redirect_to @event, alert: 'イベントの削除に失敗しました。'
+    end
   end
 
   private
@@ -49,8 +54,14 @@ class EventsController < ApplicationController
   def set_event
     @event = Event.find(params[:id])
   end
+    
+  def check_owner
+    unless @event.user == current_user
+      redirect_to events_path, alert: 'このアクションの権限がありません。'
+    end
+  end
 
   def event_params
-    params.require(:event).permit(:title, :description, :start_time,:location,:image)
+    params.require(:event).permit(:title, :description, :start_time, :location, :image)
   end
 end
