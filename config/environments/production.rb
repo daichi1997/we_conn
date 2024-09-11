eb9423acccee6def64093b15d2d6476f9e660b4c
daichi1997 <tagaaan@gmail.com>
2024-09-11 20:24:29 +0900

    アクションケーブルの編集

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,8 @@ Rails.application.configure do
   # config.action_cable.url = "wss://example.com/cable"
   # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
 
+  config.action_cable.url = 'wss://your-app-name.onrender.com/cable'
+  config.action_cable.allowed_request_origins = [ 'https://your-app-name.onrender.com', /http:\/\/your-app-name.onrender.com.*/ ]
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
