f8ccd29dde24e1f4d46132f93af924d802b50b46
daichi1997 <tagaaan@gmail.com>
2024-09-13 03:48:39 +0900

    追記

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,8 +33,9 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
+  config.public_file_server.enabled = true
   config.assets.compile = false
-
+  config.assets.digest = true
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
