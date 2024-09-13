73aeaa2dabe8e780080f2e356a0ae5f6d9a6c565
daichi1997 <tagaaan@gmail.com>
2024-09-13 12:06:51 +0900

    再リセット

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_09_034601) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_13_030353) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -66,6 +66,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_09_034601) do
     t.integer "tag_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "likes_count", default: 0, null: false
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
